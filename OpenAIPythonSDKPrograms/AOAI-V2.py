94d28d642888eea73a15de3a3c4d00f77c24e1dd
Subhasish G <63411463+SubhMSFT@users.noreply.github.com>
2024-07-13 21:28:45 +0530

    Update AOAI-V2.py

--- OpenAIPythonSDKPrograms/AOAI-V2.py
+++ OpenAIPythonSDKPrograms/AOAI-V2.py
@@ -2,7 +2,7 @@ import os
 from openai import AzureOpenAI
 
 client = AzureOpenAI(
-    azure_endpoint="https://g42-aoai.openai.azure.com/",
+    azure_endpoint="<<USE_LLM_ENDPOINT_HERE>>",
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     api_version="2024-02-15-preview"
 )
